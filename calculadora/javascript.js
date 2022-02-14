9b9bcb955b9288a323513b0b0b7d2aad63b0176a
Bruno Silva Programador <brunosilvaprogramador@gmail.com>
2022-02-14 20:48:53 -0300

    commit simple

--- calculadora/javascript.js
+++ calculadora/javascript.js
@@ -2,45 +2,111 @@ var tela = document.querySelector('input.números');
 var conta = [];
 var sit = [0];
 var operacao = [];
-var result = [0];
+var result = 0;
 
-function mais(e){
-    operacao[0] = [e.value];
+function mais(){
+    operacao[0] = '+';
     tela.value += operacao;
-    conta[0] = ''
+    conta.push('+')
+}
+function menos(){
+    operacao[0] = '-';
+    tela.value += operacao;
+    conta.push('-')
+}
+function multiplicacao(){
+    operacao[0] = '*';
+    tela.value += 'x';
+    conta.push('*')
+}
+function divisao(){
+    operacao[0] = '/';
+    tela.value += operacao;
+    conta.push('/')
 }
 
-function calc(sit){
-    if(sit == 0){
-        var oper = operacao[0] 
-        if(conta.length <= 2){
-            var calculo = eval(conta[0]+oper+conta[0]);
-            result[0] = calculo
-        }else{
-            calculo = eval(calculo+oper+conta[0]);
-            result[0] = calculo
-        }
-    }
-
-}
 
 
 function igual(){
-    sit[0] = 1
-    if(!isNaN(result[0])){
-       alert(result[0]);
+    var n1 = ''
+    var n2 = ''
+    if(operacao[0] == '+'){
+        if(sit[0] == 0){
+            for(var i = 0; i <= conta.length; i++){
+                if(conta[i] == '+'){
+                    var s = 2
+                }else{
+                    if(s == 2){
+                        n2 = n2 + conta[i] 
+                    }else{
+                        n1 = n1 + conta[i]
+                    }
+                    s=1
+                }
+            }
+            result += Number(n1) + Number(n2);
+            alert(result)
+            sit[0] = 1
+            conta = []
+        }else{
+            for(var i = 0; i <= conta.length; i++){
+                if(conta[i] == '+'){
+                    var s = 2
+                }else{
+                    if(s == 2){
+                        n1 = conta[i] 
+                    }
+                    s=1
+                }
+            }
+            result += Number(n1);
+            alert(result)
+        }
+    
+    }else if(operacao[0] == '-'){ 
+        if(sit[0] == 0){
+            result += conta[0] - conta[1];
+            alert(result)
+            sit[0] = 1
+        }else{
+            result -= conta[0];
+            alert(result)
+        }
+    }else if(operacao[0] == '*'){
+        if(sit[0] == 0){
+            result += conta[0] * conta[1];
+            alert(result)
+            sit[0] = 1
+        }else{
+            result *= conta[0];
+            alert(result)
+        }
+    
+    }else if(operacao[0] == '/'){
+        if(sit[0] == 0){
+            result += conta[0] / conta[1];
+            alert(result)
+            sit[0] = 1
+        }else{
+            result /= conta[0];
+            alert(result)
+        }
     }
+    
 }
 
 function sete(){
     tela.value += '7';
-    conta[0] = 7
-    calc(sit[0])
-    sit[0] = 0
+    if(conta[0] == ''){
+        conta.push('8')
+    }
+
+
 }
 function oito(){
     tela.value += '8';
-    conta[0] = 8
-    calc(sit[0])
-    sit[0] = 0
+    if(conta[0] == ''){
+        conta.push('8')
+    }
+
 }
